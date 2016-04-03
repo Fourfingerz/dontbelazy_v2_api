d61b6ea8926ffe2094b6fab3bea0438a8853effe
Fourfingerz <chansd5@gmail.com>
2016-04-02 20:16:54 -0400

    Swapped out database dependencies from MongoDB to MySQL and updated root SERVER.JS executable accordingly.

--- server.js
+++ server.js
@@ -1,30 +1,15 @@
 // server.js
 
 // initialize express
-var express    = require('express');
-var app        = express();
-var bodyParser = require('body-parser');
+var express        = require('express'),
+    app            = express(),
+    mysql          = require('mysql'),
+    connectionpool = mysql.createPool({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'secret',
+        database : 'rest_demo'
+    }),
+res.setHeader({ 'Content-Type: 'application/json', });
+app.get('/a', function(req, res){});
 
-// configure app to user bodyParser
-// so we can get data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;   // designate port
-
-// API ROUTES
-// =============================================================================
-var router = express.Router();         // instantiate express router
-
-// test route
-router.get('/', function(req, res) {
-  res.json({ message: 'API Endpoint is working!' });
-});
-
-// ROUTE REGISTRATION
-app.use('/a', router);
-
-// START SERVER
-// =============================================================================
-app.listen(port);
-console.log(port + ' says hi!');
